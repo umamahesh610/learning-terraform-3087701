6ab37b6f4f00322640b2a4835df8d5d152e33a1f
umamahesh610 <30557578+umamahesh610@users.noreply.github.com>
2024-06-02 13:49:53 +0200

    fix

--- main.tf
+++ main.tf
@@ -61,16 +61,16 @@ module "blog_alb" {
   target_groups = [
     {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = [{
+      targets = {
         my_target= {
           target_id = aws_instance.blog.id
           port      = 80
         }
       }
-      ]
+      
     }
   ]
 
