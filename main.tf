d936477e951345bf8f9f778c773579ce09ffb912
umamahesh610 <30557578+umamahesh610@users.noreply.github.com>
2024-06-02 09:29:42 +0200

    syntax correction

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "blog_sg" {
   ingress_rules       =  ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"] 
 
-  egress_rules       = ["all_all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"] 
 
 }
