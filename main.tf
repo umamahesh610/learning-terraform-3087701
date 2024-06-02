c108bd3d31b3ab7b6c5e33c5971fba3e61bdb0b7
umamahesh610 <30557578+umamahesh610@users.noreply.github.com>
2024-06-02 13:21:01 +0200

    new alb

--- main.tf
+++ main.tf
@@ -47,6 +47,38 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.9.0"
+  name = "${var.environment.name}-blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+
+  target_groups = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        my_target= {
+          target_id = aws_instance.blog.id
+        }
+      }
+  }
+
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+    }
+  }
+
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
+
+variable "environment"{
+  description = "Define the environment like Dev, test"
+  default     = "Dev"
+  type = object ({
+    name = "dev"
+  })
+}
